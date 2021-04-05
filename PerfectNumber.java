bfe7b208fb3570d01ca35418c69c331086045d83
LathaMurugan-code <lathamurugan8198@gmail.com>
2021-04-05 22:19:09 +0530

    programs

---
+++ PerfectNumber.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class PerfectNumber {
+	public static void main(String args[]) 
+	{
+	    int number = 28;
+	    int sum = 0;
+	    int i = 1;
+	    while(i<=number/2)
+	    {
+	        if(number%i == 0)
+	        {
+	            sum=sum+i;
+	        }
+	        i++;
+	    }
+	    if(sum == number)
+	        System.out.println("It is Perfect Number");
+	    else System.out.println("It is not Perfect Number");
+	}
+
+}
